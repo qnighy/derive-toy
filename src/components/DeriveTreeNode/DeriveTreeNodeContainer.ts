d5db405a7c9be90a5db4fcd87c366c8c150c6c65
Masaki Hara <ackie.h.gmai@gmail.com>
2019-01-02 22:36:59 +0900

    Remove DeriveTreeNodeContainer.

--- src/components/DeriveTreeNode/DeriveTreeNodeContainer.ts
+++
@@ -1,9 +0,0 @@
-import { ReduxAction, ReduxState } from "../../store";
-import { connect } from "react-redux";
-import { Dispatch } from "react";
-import { DeriveTreeNode } from "./DeriveTreeNode";
-
-export default connect(
-    (state: ReduxState) => ({ cproof: state.cproof }),
-    (dispatch: Dispatch<ReduxAction>) => ({})
-)(DeriveTreeNode)
